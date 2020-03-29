c2feb49808ead9d20263d130ed8cfb26831fadaf
LiShi <westwood23rose@gmail.com>
2020-03-28 17:11:37 -0700

    test1

--- justchat/settings.py
+++ justchat/settings.py
@@ -1,11 +1,11 @@
 import os
-import django_heroku
+# //import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9nneu#^7_aai*(#(6_qiihu-^k-+%a86&vjh=_i9#(c4^8s51n'
 DEBUG = True
 ALLOWED_HOSTS = ['justdjango-chat.herokuapp.com', '127.0.0.1', 'cs300-s20.herokuapp.com']
-django_heroku.settings(locals())
+# //django_heroku.settings(locals())
 
 INSTALLED_APPS = [
     'django.contrib.admin',
